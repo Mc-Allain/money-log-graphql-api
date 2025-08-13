d39d8d588e60dcae967171670bc8ed3b0e422108
Mc-Allain <aizen.no.testing.server@gmail.com>
2025-08-13 22:44:17 +0800

    Refactor typeDefs.js: DONE

--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -3,8 +3,7 @@ import { gql } from 'apollo-server-express';
 import envelopeTypeDefs from '../modules/envelope/envelope.typeDefs.js';
 import transactionTypeDefs from '../modules/transaction/transaction.typeDefs.js';
 
-const typeDefs = gql`
+export default gql`
   ${envelopeTypeDefs}
   ${transactionTypeDefs}
 `;
-export default typeDefs;
